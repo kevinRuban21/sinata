f1bb6ab1f2673f10492c632fd6a1ab7c17ecbf7d
Kevin Ruban <kevinruban4@gmail.com>
2025-09-24 19:56:51 +0900

    membuat tampilan responsive lanjutan

--- js/main.js
+++ js/main.js
@@ -63,3 +63,36 @@ const swiperTestimoni = new Swiper(".testimoni_swiper", {
     },
   },
 });
+
+const swiperSouvenir = new Swiper(".souvenir_swiper", {
+  loop: true,
+  spaceBetween: 50,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
